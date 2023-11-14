b6913c8617a4272f8cb8fc8af0c82549324fbf29
yihyundai@gmail.com <yihyundai@gmail.com>
2023-11-14 18:20:39 +0100

    search tutors page

---
+++ Data/Tutor/ITutorService.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Mentor.Data
+{
+    public interface ITutorService
+    {
+        TutorModel Select(int tutorID);
+        IEnumerable<TutorModel> SelectAll();
+        Task<IEnumerable<TutorModel>> SelectAllAsync();
+        IEnumerable<TutorModel> SelectAllByCompetence(int subjectID, int levelID);
+        Task<IEnumerable<TutorModel>> SelectAllByCompetenceAsync(int subjectID, int levelID);
+        Task<TutorModel> SelectAsync(int tutorID);
+    }
+}
---
+++ Data/Tutor/TutorModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Mentor.Data
+{
+    [Serializable()]
+    public class TutorModel
+    {
+        public int TUTOR_ID { get; set; } = 0;
+        public string TUTOR_FULLNAME { get; set; } = String.Empty;
+        public string TUTOR_EMAIL { get; set; } = String.Empty;
+        public string TUTOR_PHONE { get; set; } = String.Empty;
+        public string TUTOR_DESCRIPTION { get; set; } = String.Empty;
+        public int WORK_TYPE_ID { get; set; } = 0;
+        public string WORK_TYPE_NAME { get; set; } = String.Empty;
+        public int TUTOR_RATING { get; set; } = 0;
+        public List<CompetenceModel> Competences { get; set; } = new List<CompetenceModel>();
+    }
+}
