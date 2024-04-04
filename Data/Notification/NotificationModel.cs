85e2b4c8e58bb6e11da0773327162c0e0e5012f7
Błażej Maniak <s23175@pjwstk.edu.pl>
2024-04-04 16:32:28 +0200

    Notification models and code to fix

---
+++ Data/Notification/NotificationModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Mentor.Data
+{
+    public class NotificationModel
+    {
+        public int NOTIFICATION_ID { get; set; } = 0;
+        public string TEXT { get; set; } = String.Empty;
+
+    }
+}
